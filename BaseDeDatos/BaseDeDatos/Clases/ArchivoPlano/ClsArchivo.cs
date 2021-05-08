faa4dcadb5257e3cf79fc6df5f46d59fbbc285fd
diegogmartinez <79337910+diegogmartinez@users.noreply.github.com>
2021-05-07 21:47:49 -0600

    trabajo1

---
+++ BaseDeDatos/BaseDeDatos/Clases/ArchivoPlano/ClsArchivo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseDeDatos.Clases.ArchivoPlano
+{
+    class ClsArchivo
+    {
+        public string[,] LeerArchivo()
+        {
+            string[] Archivoexcel = File.ReadAllLines(@"C:\Users\alumno\Desktop\alumnos\archivoPlano.csv", Encoding.Default);
+
+            int acumulador = 0;
+
+            string[,] Matriz = new string[Archivoexcel.Length, 5];
+
+            foreach (string linea in Archivoexcel)
+            {
+                string[] datos = linea.Split(';');
+
+                Matriz[acumulador, 0] = datos[0];
+                Matriz[acumulador, 1] = datos[1];
+                Matriz[acumulador, 2] = datos[2];
+                Matriz[acumulador, 3] = datos[3];
+                Matriz[acumulador, 4] = datos[4];
+                acumulador++;
+
+            }
+
+            return Matriz;
+
+
+        }
+
+    }
+}
---
+++ BaseDeDatos/BaseDeDatos/Program.cs
@@ -0,0 +1,68 @@
+﻿using BaseDeDatos.Clases.ArchivoPlano;
+using BaseDeDatos.Clases.BaseDatos;
+using System;
+using System.Data;
+
+namespace BaseDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string respuestaUsuario;
+            Console.WriteLine("\t\t\t¿Que Base de Datos desee utilizar?");
+            Console.WriteLine("\n\t\tOracle\t\t\tMySQL\t\t\tSQL");
+            respuestaUsuario = Console.ReadLine();
+
+            string[,] archivoMatriz;
+            ClsArchivo ObjArchivo = new ClsArchivo();
+            archivoMatriz = ObjArchivo.LeerArchivo();
+            ClsConexionOracle oracle = new ClsConexionOracle();
+            ClsConexionMySQL mysql = new ClsConexionMySQL();
+            ClsConexion cn = new ClsConexion();
+
+
+
+
+
+
+            switch (respuestaUsuario)
+            {
+                case "Oracle":
+
+                    for (int i = 1; i < archivoMatriz.GetLength(0); i++)
+                    {
+
+                        DataTable dt = oracle.consultaTablaDirecta("insert into TB_ALUMNOS values (" + archivoMatriz[i, 0] + ",'" + archivoMatriz[i, 1] + "'," + archivoMatriz[i, 2] + "," + archivoMatriz[i, 3] + "," + archivoMatriz[i, 4] + ")");
+
+                    }
+
+                    break;
+
+                case "MySQL":
+
+                    for (int i = 1; i < archivoMatriz.GetLength(0); i++)
+                    {
+
+                        DataTable dt = mysql.consultaTablaDirecta("insert into tb_alumnos values (" + archivoMatriz[i, 0] + ",'" + archivoMatriz[i, 1] + "'," + archivoMatriz[i, 2] + "," + archivoMatriz[i, 3] + "," + archivoMatriz[i, 4] + ")");
+
+                    }
+
+                    break;
+
+                case "SQL":
+
+                    for (int i=1; i < archivoMatriz.GetLength(0); i++)
+                    {
+
+                        DataTable dt = cn.consultaTablaDirecta("insert into tb_alumnos values (" + archivoMatriz[i, 0] + ",'" + archivoMatriz[i, 1] + "'," + archivoMatriz[i, 2] + "," + archivoMatriz[i, 3] + "," + archivoMatriz[i, 4] + ")");
+
+
+                    }
+                   
+                    break;
+            }            
+
+        }
+    }
+}
